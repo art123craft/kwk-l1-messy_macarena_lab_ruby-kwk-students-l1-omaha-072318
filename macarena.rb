9ec84ca5ca5a7e9cba3c442d314a495acdde7e1d
art123craft <cammiebluecammie@gmail.com>
2018-07-23 20:40:43 +0000

    Done.

--- macarena.rb
+++ macarena.rb
@@ -8,22 +8,22 @@ puts ""
 # Step 1: GOOD
 puts "Step 1: Right arm out in front of you, palm facing down."
 
-# Step 2: FIXME
+# Step 2: GOOD
 puts "Step 2: Left arm out in front of you, palm facing down."
 
-# Step 3: FIXME
+# Step 3: GOOD
 puts "Step 3: Turn your right palm up."
 
-# Step 4: FIXME
+# Step 4: GOOD
 puts "Step 4: Turn your left palm up."
 
-# Step 5: FIXME
+# Step 5: GOOD
 puts "Step 5: Place your right hand on your left shoulder."
 
-# Step 6: FIXME
+# Step 6: GOOD
 puts "Step 6: Place your left hand on your right shoulder."
 
-# Step 7: FIXME
+# Step 7: GOOD
 puts "Step 7: Bring your right hand up and around to the back of your head."
-# Step 8: FIXME
+# Step 8: GOOD
 puts "Step 8: Bring your left hand up and around to the back your head."
